b2073374636965290c9d5520a7f2338028bf5395
justusgeslani <jagblue9@gmail.com>
2024-03-03 15:27:53 -0500

    Updated Login Component

--- frontend/src/app/login-account/login-account.component.ts
+++ frontend/src/app/login-account/login-account.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import { ModalReference } from '@developer-partners/ngx-modal-dialog';
 
+export interface User {
+  username: string,
+  password: string,
+}
 @Component({
   selector: 'app-login-account',
   templateUrl: './login-account.component.html',
   styleUrls: ['./login-account.component.css']
 })
 export class LoginAccountComponent {
+
+  public user: User = {
+    username: '',
+    password: '',
+  };
+
+  constructor(private readonly _modalReference: ModalReference<User>) {
+    
+  }
+
+  public createAccount(): void {
+    
+  }
+
+  public cancel(): void {
+
+    this._modalReference.cancel();
+  }
+
+  public saveData(): void {
+
+    this._modalReference.closeSuccess(this.user);
+  }
 
 }

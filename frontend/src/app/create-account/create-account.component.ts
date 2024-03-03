9e8ecb2f0f7a4c2f2360fc0b642a291cc2f3cd03
justusgeslani <jagblue9@gmail.com>
2024-03-03 15:50:41 -0500

    Updated Create Account Component

--- frontend/src/app/create-account/create-account.component.ts
+++ frontend/src/app/create-account/create-account.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { ModalReference } from '@developer-partners/ngx-modal-dialog';
 
+export interface newUser {
+
+  fullName: string,
+  username: string,
+  password: string,
+
+}
 @Component({
   selector: 'app-create-account',
   templateUrl: './create-account.component.html',
   styleUrls: ['./create-account.component.css']
 })
 export class CreateAccountComponent {
+
+  public newuser: newUser = {
+    fullName: '',
+    username: '',
+    password: '',
+  };
+
+  constructor(private readonly _modalReference: ModalReference<newUser>) {}
+
+  public cancel(): void {
+
+    this._modalReference.cancel();
+  }
+
+  public saveData(): void {
+
+    this._modalReference.closeSuccess(this.newuser);
+  }
 
 }

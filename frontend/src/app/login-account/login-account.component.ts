8b14ac6f182daf8872c568872d92490361ff1687
justusgeslani <jagblue9@gmail.com>
2024-03-07 18:22:41 -0500

    Updated Login Component

--- frontend/src/app/login-account/login-account.component.ts
+++ frontend/src/app/login-account/login-account.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { ModalReference } from '@developer-partners/ngx-modal-dialog';
 import { ModalService } from '@developer-partners/ngx-modal-dialog';
 import { CreateAccountComponent } from '../create-account/create-account.component';
+import { HttpClient, HttpHandler, HttpRequest } from '@angular/common/http';
+import { NgForm } from '@angular/forms';
 
 export interface User {
   username: string,
@@ -14,21 +16,48 @@ export interface User {
 })
 export class LoginAccountComponent {
 
+  title="Login Site";
+  allLogins: LoginTest[] = []
+
+  constructor(private http: HttpClient, private readonly _modalReference: ModalReference<User>, private readonly _modalService: ModalService) {
+    
+  }
+
+  ngOnInit(){
+    this.getLogins();
+  }
+
+  loginUser(f: NgForm){
+
+    let loginUsername = f.value.loginUsername
+    let loginPassword = f.value.loginPassword
+
+    console.log(this.title)
+
+    let loginTest = {
+      "Username": loginUsername,
+      "Password": loginPassword,
+    }
+
+  };
+
+  getLogins() {
+
+  }
+
+
   public user: User = {
     username: '',
     password: '',
   };
 
-  constructor(private readonly _modalReference: ModalReference<User>, private readonly _modalService: ModalService) {
-    
-  }
 
   public createAccount(): void {
     
     this._modalService.show<User>(CreateAccountComponent, {
       title: 'Become a CinemaFusion Member',
       mode: 'disableFullScreen',
-      type: 'warning',
+      type: 'default',
     })
   }
 
@@ -40,6 +69,17 @@ export class LoginAccountComponent {
   public saveData(): void {
 
     this._modalReference.closeSuccess(this.user);
+  }
+
+}
+
+class LoginTest {
+  LoginUsername: string;
+  LoginPassword: string;
+
+  constructor(loginUsername:string, loginPassword:string) {
+    this.LoginUsername=loginUsername;
+    this.LoginPassword=loginPassword;
   }
 
 }

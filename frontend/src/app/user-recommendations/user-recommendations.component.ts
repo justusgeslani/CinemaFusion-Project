07b4e182cca3fbb90067fa4c1924d607953d0efa
Charlene Creighton <creightonc@ufl.edu>
2024-03-18 18:26:24 -0400

    Front end component added for user recommendation

---
+++ frontend/src/app/user-recommendations/user-recommendations.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-recommendations',
+  templateUrl: './user-recommendations.component.html',
+  styleUrls: ['./user-recommendations.component.css']
+})
+export class UserRecommendationsComponent {
+
+}

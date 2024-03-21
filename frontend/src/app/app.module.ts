8ab5c1892769825abfd463013cdd6bd8d8bbd939
jnnb515 <hbao@ufl.edu>
2024-03-20 20:06:33 -0400

    Create front page

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { FrontPageComponent } from './front-page/front-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrontPageComponent
   ],
   imports: [
     BrowserModule,	  

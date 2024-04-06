761e02313c804c360e64265b76c64b94e2a19beb
jnnb515 <hbao@ufl.edu>
2024-04-06 16:19:27 -0400

    changes on quiz and userRecommendations component

--- frontend/src/app/quiz/quiz.component.ts
+++ frontend/src/app/quiz/quiz.component.ts
@@ -1,48 +1,18 @@
-// import { Component } from '@angular/core';
-
-// @Component({
-//   selector: 'app-quiz',
-//   //standalone: true,
-//   //imports: [],
-//   templateUrl: './quiz.component.html',
-//   styleUrls: ['./quiz.component.css']
-// })
-// export class QuizComponent {
-
-//   constructor() { }
-
-//   getRecommendation() {
-//     // Implement logic to get recommendation
-//     // and display the result in the 'result' div
-//   }
-
-// }
-
+import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { ModalService, ModalConfig } from '@developer-partners/ngx-modal-dialog'; // Ensure to import ModalConfig
+import { UserRecommendationsComponent } from '../user-recommendations/user-recommendations.component';
 
 @Component({
   selector: 'app-quiz',
   templateUrl: './quiz.component.html',
   styleUrls: ['./quiz.component.css']
 })
-export class QuizComponent implements OnInit {
-  
-  constructor(private http: HttpClient) {
+export class QuizComponent {
+  constructor(private http: HttpClient) {}
 
-  }
-    
-  ngOnInit(): void {
-    // Logic to be executed on component initialization
-    console.log('QuizComponent ngOnInit called');
-  }
-
-  getRecommendation(quizForm: NgForm) {
-    // Logic to get recommendation
-    // Get form values
-    
-    const FormValues = {
+  getRecommendation(quizForm: any) {
+    const formValues = {
       weather: quizForm.value.weather,
       feelings: quizForm.value.feelings,
       gender: quizForm.value.gender,
@@ -50,31 +20,104 @@ export class QuizComponent implements OnInit {
       time: quizForm.value.time,
       when: quizForm.value.when,
     };
-    
 
-    // console.log(FormValues)
-    this.http.post('http://localhost:8080/movies/byquiz/get', JSON.stringify(FormValues)).subscribe((moviesList: any)=> {
-        if (200) {
-          var movie = JSON.parse(moviesList)
-          console.log(movie)  //parse 'movie' to print on UI
-        }
+    this.http.post<any>('http://localhost:8080/movies/byquiz/get', JSON.stringify(formValues))
+      .subscribe((movie: any) => {
+        this.showRecommendationsModal(movie);
       }, (error) => {
-        if (error.status === 404) {
-          alert('Resource not found.');
-        }
-        else if (error.status === 403) {
-          alert('Forbidden Access to Resource');
-        }
-        else if (error.status === 500) {
-          alert('Server down.');
-        }
-        else if (error.status === 502) {
-          alert('Bad gateway.');
-        }
-      }
-      
-    );
+        console.error('Error:', error);
+      });
+  }
 
+  private showRecommendationsModal(movie: any): void {
+    const modalConfig: ModalConfig<UserRecommendationsComponent> = { 
+      title: 'Movie Recommendation',
+      type: 'default',
+      mode: 'disableFullScreen',
+      data: movie
+    };
+
+    this._modalService.show<UserRecommendationsComponent>(UserRecommendationsComponent, modalConfig); 
   }
 }
+
+
+
+
+
+
+  
+  
+  
+  // private showRecommendationsModal(movie: any): void {
+  //   this._modalService.show<Movie>(UserRecommendationsComponent, {
+  //     title: 'Movie Recommendation',
+  //     type: 'default',
+  //     mode: 'disableFullScreen',  
+  //   })
+  // }
+}
+
+// import { HttpClient } from '@angular/common/http';
+// import { Component, OnInit } from '@angular/core';
+// import { NgForm } from '@angular/forms';
+
+// @Component({
+//   selector: 'app-quiz',
+//   templateUrl: './quiz.component.html',
+//   styleUrls: ['./quiz.component.css']
+// })
+// export class QuizComponent implements OnInit {
+  
+//   constructor(private http: HttpClient) {
+
+//   }
+    
+//   ngOnInit(): void {
+//     // Logic to be executed on component initialization
+//     console.log('QuizComponent ngOnInit called');
+//   }
+
+//   getRecommendation(quizForm: NgForm) {
+//     // Logic to get recommendation
+//     // Get form values
+    
+//     const FormValues = {
+//       weather: quizForm.value.weather,
+//       feelings: quizForm.value.feelings,
+//       gender: quizForm.value.gender,
+//       age: quizForm.value.age,
+//       time: quizForm.value.time,
+//       when: quizForm.value.when,
+//     };
+    
+
+//     // console.log(FormValues)
+//     this.http.post('http://localhost:8080/movies/byquiz/get', JSON.stringify(FormValues)).subscribe((moviesList: any)=> {
+//         if (200) {
+//           var movie = JSON.parse(moviesList)
+//           console.log(movie)  //parse 'movie' to print on UI
+//         }
+//       }, (error) => {
+//         if (error.status === 404) {
+//           alert('Resource not found.');
+//         }
+//         else if (error.status === 403) {
+//           alert('Forbidden Access to Resource');
+//         }
+//         else if (error.status === 500) {
+//           alert('Server down.');
+//         }
+//         else if (error.status === 502) {
+//           alert('Bad gateway.');
+//         }
+//       }
+      
+//     );
+
+//   }
+// }
+
+
+
 
